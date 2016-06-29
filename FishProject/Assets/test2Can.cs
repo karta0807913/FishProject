010783671a97f40c346efe44e56f5d9aafe33e0c
karta0807913 <karta0807913@gmail.com>
2016-06-29 23:04:06 +0800

    Learn it

---
+++ FishProject/Assets/test2Can.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test2Can : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        this.transform.Rotate(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0);
+	}
+}
